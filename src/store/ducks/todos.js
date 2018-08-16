f9e3dc0e1fb5a93b0c7b893b00b83011577eaeef
Robson Dias <robsondiaz@gmail.com>
2018-08-15 22:15:31 -0300

    Duck Pattern

--- src/store/ducks/todos.js
+++ src/store/ducks/todos.js
@@ -1,20 +1,26 @@
+export const Types = {
+    ADD: "todos/ADD",
+    TOGGLE: "todos/TOGGLE",
+    REMOVE: "todos/REMOVE",
+}
+
 const INITIAL_STATE = [];
 
 export default function todos(state = INITIAL_STATE, action) {
 
     switch(action.type) {
-        case "ADD_TODO":
+        case Types.ADD:
             return [
                 ...state,
                 { id: Math.random(), text : action.payload.text, complete: false }
             ]
         
-        case "TOGGLE_TODO":
+        case Types.TOGGLE:
             return state.map(
                 todo => todo.id === action.payload.id ? { ...todo, complete: !todo.complete } : todo
             )
 
-        case "REMOVE_TODO":
+        case Types.REMOVE:
             return state.filter(todo => todo.id !== action.payload.id)
 
         default:
@@ -22,4 +28,27 @@ export default function todos(state = INITIAL_STATE, action) {
         
     }
 
+}
+
+export const Creators = {
+    addTodo: text => ({
+        type: Types.ADD,
+        payload: {
+            text
+        }
+    }),
+    
+    toggleTodo: id => ({
+        type: Types.TOGGLE,
+        payload: {
+            id
+        }
+    }),
+    
+    removeTodo: id => ({
+        type: Types.REMOVE,
+        payload: {
+            id
+        }
+    })
 }
